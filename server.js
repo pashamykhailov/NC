9c7dfe494d7588360ed66e1a510091e524115596
Mykhailov Pavel <mykhailovpm@gmail.com>
2017-10-31 18:41:44 +0700

    add full project

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+
+app.use((req, res, next) => {
+  res.render('maintance.hbs', {
+    pageTitle: "Maintance page",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: "Home page",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About page",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'You have an error'
+  });
+});
+
+app.listen(port, () => {
+  console.log('App running successfully on port:', port);
+});
