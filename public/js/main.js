05615ed84c839a7a44b5007beea908b79eaf2c3c
Mykhailov Pavel <mykhailovpm@gmail.com>
2017-11-04 22:59:57 +0700

    finished

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,29 @@
 (function () {
-  console.log('hello world');
+  let downloadButton = document.getElementById('download');
+  let input = document.getElementById('input');
+  let save = document.getElementById('save');
+  let loader = document.getElementsByClassName('loader')[0];
+  let response;
+
+  downloadButton.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+      loader.style.display = 'block';
+      let XHR = new XMLHttpRequest();
+      XHR.onload = (res) => {
+        loader.style.display = 'none';
+        save.style.display = 'block';
+        console.log('res ', res);
+        response = JSON.parse(res.currentTarget.response);
+      };
+      XHR.open("get", `/get-image?url=${input.value}`, true);
+      XHR.send();
+    }
+  });
+
+  save.addEventListener('click', function () {
+    let link = document.createElement("a");
+    link.setAttribute("href", response.result.image_url);
+    link.setAttribute("download", 'ninja-copy');
+    link.click();
+  });
 })();
--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const path = require('path');
+const request = require('request');
 
 let app = express();
+function getRequestInsta(url) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: `http://api.ninja-miners.com/instagram?url=${url}`,
+      json: true
+    }, (error, response, body) => {
+      if (error) {
+        reject('error occurs ', error);
+      } else if (body.status === 'ZERO_RESULTS') {
+        reject('ZERO_RESULTS ');
+      } else if (body.result.image_url) {
+        console.log('body ',body);
+        resolve(body);
+      } else  {
+        reject('please check is your url is correct ', url);
+      }
+    });
+  });
+}
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname +'/html/index.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/html/index.html'))
 });
 
+app.get('/get-image', (req, res) => {
+  console.log('req ', req.query);
+  getRequestInsta(req.query.url).then((success) => {
+    console.log('result ', success);
+    res.send(success);
+  }, (error) => {
+    res.send(error);
+  });
+});
 
 app.listen(port, () => {
   console.log('App running successfully on port:', port);
