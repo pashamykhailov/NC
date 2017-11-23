f5b2d203f6fd319c9a3fe5571b8bf9f811b178f1
Mykhailov Pavel <mykhailovpm@gmail.com>
2017-11-23 22:23:18 +0700

    add new functionality

--- public/js/main.js
+++ public/js/main.js
@@ -28,24 +28,43 @@
     data: {
       url: '',
       loaderStatus: false,
-      previewImageArray: []
-
+      previewImageArray: [],
+      privateAccount: false
     },
     methods: {
       getAllPhotos() {
+        this.previewImageArray = [];
         this.loaderStatus = true;
         this.$http.get(`/get-image?url=${this.url}`).then((success) => {
           this.loaderStatus = false;
           let responseResult = success.body.result;
           if (responseResult.posts) {
+            this.privateAccount = responseResult.private;
             this.previewImageArray = this.previewImageArray.concat(responseResult.posts);
           } else if (responseResult.item) {
+            this.privateAccount = responseResult.private ? responseResult.private : false;
             this.previewImageArray.push(responseResult.item);
           }
         }, (error) => {
           console.log(error);
           this.loaderStatus = false;
         });
+      },
+      downloadSinglePhoto(src, video) {
+        let obj;
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext("2d");
+        if (!video) {
+          obj =  new Image();
+          obj.setAttribute('crossOrigin', 'anonymous');
+          obj.setAttribute('src', src);
+          obj.onload = () => {
+            canvas.width = obj.width;
+            canvas.height = obj.height;
+            ctx.drawImage(obj, 0, 0);
+            download(canvas.toDataURL(), 'ninja-copy', 'image/jpeg');
+          };
+        }
       }
     }
   });
