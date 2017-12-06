d28458e375f47b7959e83055aafeba854d131ec3
Mykhailov Pavel <mykhailovpm@gmail.com>
2017-12-07 04:32:06 +0700

    add test download file

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const path = require('path');
 const request = require('request');
+const fs = require('fs');
+
+function download(uri, filename, callback){
+request.head(uri, function(err, res){
+console.log('content-type:', res.headers['content-type']);
+console.log('content-length:', res.headers['content-length']);
+
+request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+});
+}
+
+download('https://www.google.com/images/srpr/logo3w.png', 'google.png', function(){
+console.log('done');
+});
 
 let app = express();
 
